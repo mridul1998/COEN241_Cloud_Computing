8f6edcc0d826212b3b0d1f8edb42d89b9343be78
mridul1998 <33255464+mridul1998@users.noreply.github.com>
2022-10-17 15:49:47 -0700

    Delete Docker_2000_Cpu_test_mode.sh

--- Docker_2000_Cpu_test_mode.sh
+++
@@ -1,5 +0,0 @@
-sysbench --test=cpu --cpu-max-prime=2000 --time=30 run
-sysbench --test=cpu --cpu-max-prime=2000 --time=30 run
-sysbench --test=cpu --cpu-max-prime=2000 --time=30 run
-sysbench --test=cpu --cpu-max-prime=2000 --time=30 run
-sysbench --test=cpu --cpu-max-prime=2000 --time=30 run
